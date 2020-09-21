8f58126ecaf2a391b39b70b627ff4fe616340f3e
ManshaS24 <68643838+ManshaS24@users.noreply.github.com>
2020-09-21 18:04:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock;
+function preload()
+{
+  //load images here
+  dog = loadImage("images/dogImg.png")
+  happyDog = loadImage("images/dogImg1.png")
+}
+
+function setup() {
+	createCanvas(500, 500);
+  var Dog = createSprite (250,250)
+  Dog.addImage(dog)
+  foodStock = database.ref('food'),
+  foodStock.on("value", readStock)
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeSAtock(x){
+  if(x<=0){
+    x = 0
+  }else{
+    x = x-1
+  }
+  database.ref('/').update({
+    food:x
+  })
+}
+function draw() { 
+  background(46.139,87) 
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS)
+    Dog.addImage(dogHappy);
+  }
+  drawSprites();
+  //add styles here
+  textSize(22);
+  fill("white")
+  //stroke
+  text("Press up arrow key to feed the dog",125,125 )
+}
+
+
+
